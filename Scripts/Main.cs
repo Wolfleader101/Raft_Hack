91ba88ebc9cb695e0382087fe20257cb258e9d72
Wolfleader <james.morgan@iinet.net.au>
2021-06-03 18:12:34 +0800

    Updated Drawer

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -5,41 +5,30 @@ namespace Raft_Hack.Scripts
 {
 	class Main : MonoBehaviour
 	{
-		//private bool m_Initialized = false;
 		private Player m_Player;
 		private PlayerStats m_PlayerStats;
 		private Stat_Health m_PlayerHealth;
-		//public static ConsoleWriter _console;
+		public static ConsoleWriter _console;
 
 		public void Start()
 		{
-			//_console = new ConsoleWriter();
+			_console = new ConsoleWriter();
 
 
-			//_console.Log("Cheat Initialized", LOG_TYPE.WARNING);
+			_console.Log("Cheat Initialized", LOG_TYPE.WARNING);
 			m_Player = FindObjectOfType<Player>();
-			if(m_Player == null)
-			{
-				//m_Initialized = false;
-			}
 
 			m_PlayerStats = m_Player?.GetComponent<PlayerStats>();
 			m_PlayerHealth = m_PlayerStats?.stat_health;
 
-			//_console.Log($"Player HP: {m_PlayerStats?.stat_health.Value} \nMax HP: {m_PlayerStats?.stat_health.Max}", LOG_TYPE.INFO);
+			_console.Log($"Player HP: {m_PlayerStats?.stat_health.Value} \nMax HP: {m_PlayerStats?.stat_health.Max}", LOG_TYPE.INFO);
 		}
 		public void Update()
 		{
-			//while(!m_Initialized)
-			//{
-			//	m_Player = FindObjectOfType<Player>();
-			//	if (m_Player) m_Initialized = true;
-			//}
 
 			if(Input.GetKeyDown(KeyCode.UpArrow))
 			{
 				IncreaseHealth();
-				//_console.Log($"New Player HP: {m_PlayerStats?.stat_health.Value}", LOG_TYPE.INFO);
 			}
 
 			if (Input.GetKeyDown(KeyCode.Delete))
@@ -50,10 +39,7 @@ namespace Raft_Hack.Scripts
 
 		public void OnGUI()
 		{
-			GUIStyle style = new GUIStyle();
-			style.fontSize = 20;
-			style.normal.textColor = Color.red;
-			GUI.Label(new Rect(Screen.width / 2, 10, 150f, 50f), "Raft Hack", style);
+			Drawer.DrawText("Raft Trainer", new Vector2(Screen.width / 2, 10), false, 20, FontStyle.Bold, Color.cyan);
 
 			MenuMaker.MakeBox("Raft Trainer", new Vector2(10, 10), new Vector2(100,100));
 			MenuMaker.MakeButton("Increase HP", new Vector2(20, 40), new Vector2(80, 20), IncreaseHealth)
@@ -61,12 +47,13 @@ namespace Raft_Hack.Scripts
 
 		public void OnDestroy()
 		{
-			//_console.Destroy();
+			_console.Destroy();
 		}
 
 		private void IncreaseHealth()
 		{
 			m_PlayerStats.stat_health.Value += 15f;
+			_console.Log($"New Player HP: {m_PlayerStats?.stat_health.Value}", LOG_TYPE.INFO);
 		}
 	}
 }
--- Utils/Drawer.cs
+++ Utils/Drawer.cs
@@ -3,17 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Raft_Hack.Utils
 {
 	class Drawer
 	{
+		public static GUIStyle TextStyle { get; set; } = new GUIStyle(GUI.skin.label);
+
+		private const int _defaultFontSize = 14;
+		private const FontStyle _defaultFontStyle = FontStyle.Normal;
+
+		public static void DrawText(string name, Vector2 position)
+		{
+			_DrawText(name, position, false, _defaultFontSize, _defaultFontStyle, null);
+		}
+
+		public static void DrawText(string name, Vector2 position, bool centered)
+		{
+			_DrawText(name, position, centered, _defaultFontSize, _defaultFontStyle, null);
+		}
+
+
+		public static void DrawText(string name, Vector2 position, bool centered, int fontSize = _defaultFontSize)
+		{
+			_DrawText(name, position, centered, fontSize, _defaultFontStyle, null);
+		}
+
+
+		public static void DrawText(string name, Vector2 position, bool centered, int fontSize = _defaultFontSize, Color? color = null)
+		{
+			_DrawText(name, position, centered, fontSize, _defaultFontStyle, color);
+		}
+
+
+		public static void DrawText(string name, Vector2 position, bool centered, int fontSize = _defaultFontSize, FontStyle fontStyle = _defaultFontStyle)
+		{
+			_DrawText(name, position, centered, fontSize, fontStyle, null);
+		}
+
+		public static void DrawText(string name, Vector2 position, bool centered, int fontSize = _defaultFontSize, FontStyle fontStyle = _defaultFontStyle, Color? color = null)
+		{
+			_DrawText(name, position, centered, fontSize, fontStyle, color);
+		}
+
+
+		private static void _DrawText(string name, Vector2 position, bool centered, int fontSize, FontStyle fontStyle, Color? color)
+		{
+			TextStyle.normal.textColor = color.GetValueOrDefault(Color.black);
+			TextStyle.fontSize = fontSize;
+			TextStyle.fontStyle = fontStyle;
+
+
+			var label = new GUIContent(name);
+			var size = TextStyle.CalcSize(label);
+			var _pos = centered ? position - size / 2f : position;
+
+			GUI.Label(new Rect(_pos, size), "Raft Hack", TextStyle);
+		}
 
 		public static void DrawBox() { }
-		public static void DrawString()
-		{
-
-		}
 
 		public static void DrawLine() { }
 	}
